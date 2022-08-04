78a2838bf643acda54f91318b7c098236a6130df
EADN_PC <salahtobok1@gmail.com>
2022-08-04 10:08:32 +0100

    update repo

--- adssysadmin_front/src/main/java/com/adssys/adssysadmin_front/JAXRSConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.adssys.adssysadmin_front;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("resources")
-public class JAXRSConfiguration extends Application {
-
-}
